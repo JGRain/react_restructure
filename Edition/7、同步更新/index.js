fe28057bf752f2e9420a35af00486c29f965f794
Rain <caixinemail@sina.com>
2022-01-06 20:13:38 +0800

    7、同步更新

---
+++ Edition/7、同步更新/index.js
@@ -0,0 +1,36 @@
+import React from './react';
+import ReactDOM from './react-dom';
+
+/**
+ * 1、 如何绑定事件
+ * 2、如何实现组件更新
+ * 3、如何实现组件异步更新
+ */
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'zhufeng', num: 0 };
+  }
+  handelClick = () => {
+    this.setState({
+      num: this.state.num + 1,
+    });
+    console.log(this.state.num);
+
+    this.setState({
+      num: this.state.num + 1,
+    });
+    console.log(this.state.num);
+  };
+  render() {
+    return (
+      <div>
+        <h1>hello, {this.props.name}</h1>
+        <h2> {this.state.num}</h2>
+        <button onClick={this.handelClick}>+</button>
+      </div>
+    );
+  }
+}
+let element = <Clock name="zhufeng" />;
+ReactDOM.render(element, document.getElementById('root'));
