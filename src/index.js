abc305d34bcd8a0f4cbe93c7243d33f94472e452
huowu <huowu@ywwl.com>
2022-02-09 17:38:02 +0800

    feat：useReducer

--- src/index.js
+++ src/index.js
@@ -1,109 +1,77 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 let hookStates = []
 let hookIndex = 0
 
-function useState(initalState) {
-	hookStates[hookIndex] = hookStates[hookIndex] || initalState
+function useReducer(reducer, initalState, init) {
+	hookStates[hookIndex] =
+		hookStates[hookIndex] || (init ? init(initalState) : initalState)
 	let currentIndex = hookIndex
-	function setState(newState) {
-		hookStates[currentIndex] = newState
+	function dispatch(actions) {
+		hookStates[currentIndex] = reducer
+			? reducer(hookStates[currentIndex], actions)
+			: actions
 		render()
 	}
-	return [hookStates[hookIndex++], setState]
+	return [hookStates[hookIndex++], dispatch]
 }
 
-function useMemo(factory, deps) {
-	if (hookStates[hookIndex]) {
-		let [lastMemo, lastDeps] = hookStates[hookIndex]
-		let same = deps.every((item, index) => item === lastDeps[index])
-		if (same) {
-			hookIndex++
-			return lastMemo
-		} else {
-			let newMeno = factory()
-			hookStates[hookIndex++] = [newMeno, deps]
-			return newMeno
-		}
-	} else {
-		//如果取不到
-		let newMeno = factory()
-		hookStates[hookIndex++] = [newMeno, deps]
-		return newMeno
-	}
-}
-
-function useCallback(callback, deps) {
-	if (hookStates[hookIndex]) {
-		let [lastCallback, lastDeps] = hookStates[hookIndex]
-		let same = deps.every((item, index) => item === lastDeps[index])
-		if (same) {
-			hookIndex++
-			return lastCallback
-		} else {
-			hookStates[hookIndex++] = [callback, deps]
-			return callback
-		}
-	} else {
-		//如果取不到
-		hookStates[hookIndex++] = [callback, deps]
-		return callback
-	}
-}
-
-function memo(OldComponent) {
-	return class extends React.PureComponent {
-		render() {
-			return <OldComponent {...this.props} />
-		}
-	}
+// useState 只是一个简化版的useReducer的语法糖
+function useState(initalState) {
+	// reducer就是一个状态变更的转换函数
+	return useReducer(null, initalState)
 }
 
 /**
- * useCallback
- * useMemo
- *
- *
+ * 处理器， 状态变更函数
+ * 接受一个老状态和一个action（动作），返回一个新状态
+ * @param {*} state
+ * @param {*} action
  */
-let Child = ({ data, handleClick }) => {
-	console.log('Child render')
-	return <button onClick={handleClick}>{data.number}</button>
+function reducer(state, action) {
+	switch (action.type) {
+		case 'ADD':
+			return { number: state.number + 1 }
+		case 'MINUS':
+			return { number: state.number - 1 }
+		default:
+			return state
+	}
 }
 
-Child = memo(Child)
-function App() {
-	console.log('App render')
-	const [number, setNumber] = useState(0)
+let initialState = 0
+function init(initialState) {
+	return { number: initialState }
+}
 
-	let data = useMemo(
-		() => ({
-			number,
-		}),
-		[number]
-	)
-
-	let handleClick = useCallback(() => {
-		setNumber((x) => x + 1)
-	}, [number])
-
-	const [name, setName] = useState('zhufeng')
-
+function Counter() {
+	const [count, setCount] = useState(0)
+	let [state, dispatch] = useReducer(reducer, initialState, init)
 	return (
 		<div>
-			<input
-				value={name}
-				onChange={(e) => {
-					setName(e.target.value)
+			<p>{state.number}</p>
+			<button
+				onClick={() => {
+					dispatch({ type: 'ADD' })
 				}}
-			/>
-			<Child data={data} handleClick={handleClick} />
+			>
+				+
+			</button>
+			<p>{count}</p>
+
+			<button
+				onClick={() => {
+					setCount(count + 1)
+				}}
+			>
+				+count
+			</button>
 		</div>
 	)
 }
 function render() {
 	hookIndex = 0
-	ReactDOM.render(<App />, document.getElementById('root'))
+	ReactDOM.render(<Counter />, document.getElementById('root'))
 }
 render()
 
